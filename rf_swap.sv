706ce16d7b319a831ba9432e10fbb548acf9bd47
LamKelly <159173421+LamKelly@users.noreply.github.com>
2026-03-11 22:31:57 -0500

    V1

--- rf_swap.sv
+++ rf_swap.sv
@@ -1,233 +1,152 @@
-module rf_swap_next (
-    input  logic [3:0] cur0, cur1, cur2, cur3, cur4, cur5, cur6, cur7,
-    input  logic       init,
-    input  logic       swapnow,
-    input  logic [2:0] x,
-    input  logic [2:0] y,
-    output logic [3:0] nxt0, nxt1, nxt2, nxt3, nxt4, nxt5, nxt6, nxt7
-);
+module rf_s_nxt (
+    input logic [3:0] c0,c1,c2,c3,c4,c5,c6,c7,
+    input logic init, swapnow, [2:0] x, [2:0] y,
+    output logic [3:0] nxt0, nxt1, nxt2, nxt3, nxt4, nxt5, nxt6, nxt7);
 
     always_comb begin
-        // default = hold present values
-        nxt0 = cur0;
-        nxt1 = cur1;
-        nxt2 = cur2;
-        nxt3 = cur3;
-        nxt4 = cur4;
-        nxt5 = cur5;
-        nxt6 = cur6;
-        nxt7 = cur7;
+        nxt0 = c0; nxt1 = c1; nxt2 = c2; 
+        nxt3 = c3; nxt4 = c4; nxt5 = c5; 
+        nxt6 = c6; nxt7 = c7;
 
-        // Behavior 1: initialization
+        // BEHAVIOR 1
         if (init) begin
-            nxt0 = 4'd0;
-            nxt1 = 4'd1;
-            nxt2 = 4'd2;
-            nxt3 = 4'd3;
-            nxt4 = 4'd4;
-            nxt5 = 4'd5;
-            nxt6 = 4'd6;
-            nxt7 = 4'd7;
+            nxt0 = 4'd0;    nxt1 = 4'd1;
+            nxt2 = 4'd2;    nxt3 = 4'd3;
+            nxt4 = 4'd4;    nxt5 = 4'd5; 
+            nxt6 = 4'd6;     nxt7 = 4'd7;
         end
 
-        // Behavior 2: swapping
+        // BEHAVIOR 2
         else if (swapnow) begin
-            // first: write x position with value from y
             case (x)
                 3'd0: begin
                     case (y)
-                        3'd0: nxt0 = cur0;
-                        3'd1: nxt0 = cur1;
-                        3'd2: nxt0 = cur2;
-                        3'd3: nxt0 = cur3;
-                        3'd4: nxt0 = cur4;
-                        3'd5: nxt0 = cur5;
-                        3'd6: nxt0 = cur6;
-                        3'd7: nxt0 = cur7;
+                        3'd0: nxt0 =c0;    3'd1: nxt0 =c1;
+                        3'd2: nxt0 =c2;    3'd3: nxt0 =c3;
+                        3'd4: nxt0 =c4;    3'd5: nxt0 =c5;
+                        3'd6: nxt0 =c6;    3'd7: nxt0 =c7;
                     endcase
                 end
                 3'd1: begin
                     case (y)
-                        3'd0: nxt1 = cur0;
-                        3'd1: nxt1 = cur1;
-                        3'd2: nxt1 = cur2;
-                        3'd3: nxt1 = cur3;
-                        3'd4: nxt1 = cur4;
-                        3'd5: nxt1 = cur5;
-                        3'd6: nxt1 = cur6;
-                        3'd7: nxt1 = cur7;
+                        3'd0: nxt1= c0;    3'd1: nxt1= c1;
+                        3'd2: nxt1= c2;    3'd3: nxt1= c3;
+                        3'd4: nxt1= c4;    3'd5: nxt1= c5;
+                        3'd6: nxt1= c6;    3'd7: nxt1= c7;
                     endcase
                 end
                 3'd2: begin
                     case (y)
-                        3'd0: nxt2 = cur0;
-                        3'd1: nxt2 = cur1;
-                        3'd2: nxt2 = cur2;
-                        3'd3: nxt2 = cur3;
-                        3'd4: nxt2 = cur4;
-                        3'd5: nxt2 = cur5;
-                        3'd6: nxt2 = cur6;
-                        3'd7: nxt2 = cur7;
+                        3'd0: nxt2 = c0;    3'd1: nxt2 = c1;
+                        3'd2: nxt2 = c2;    3'd3: nxt2 = c3;
+                        3'd4: nxt2 = c4;    3'd5: nxt2 = c5;
+                        3'd6: nxt2 = c6;    3'd7: nxt2 = c7;
                     endcase
                 end
                 3'd3: begin
                     case (y)
-                        3'd0: nxt3 = cur0;
-                        3'd1: nxt3 = cur1;
-                        3'd2: nxt3 = cur2;
-                        3'd3: nxt3 = cur3;
-                        3'd4: nxt3 = cur4;
-                        3'd5: nxt3 = cur5;
-                        3'd6: nxt3 = cur6;
-                        3'd7: nxt3 = cur7;
+                        3'd0: nxt3 = c0;    3'd1: nxt3 = c1;
+                        3'd2: nxt3 = c2;    3'd3: nxt3 = c3;
+                        3'd4: nxt3 = c4;    3'd5: nxt3 = c5;
+                        3'd6: nxt3 = c6;    3'd7: nxt3 = c7;
                     endcase
                 end
                 3'd4: begin
                     case (y)
-                        3'd0: nxt4 = cur0;
-                        3'd1: nxt4 = cur1;
-                        3'd2: nxt4 = cur2;
-                        3'd3: nxt4 = cur3;
-                        3'd4: nxt4 = cur4;
-                        3'd5: nxt4 = cur5;
-                        3'd6: nxt4 = cur6;
-                        3'd7: nxt4 = cur7;
+                        3'd0: nxt4 = c0;    3'd1: nxt4 = c1;
+                        3'd2: nxt4 = c2;    3'd3: nxt4 = c3;
+                        3'd4: nxt4 = c4;    3'd5: nxt4 = c5;
+                        3'd6: nxt4 = c6;    3'd7: nxt4 = c7;
                     endcase
                 end
                 3'd5: begin
                     case (y)
-                        3'd0: nxt5 = cur0;
-                        3'd1: nxt5 = cur1;
-                        3'd2: nxt5 = cur2;
-                        3'd3: nxt5 = cur3;
-                        3'd4: nxt5 = cur4;
-                        3'd5: nxt5 = cur5;
-                        3'd6: nxt5 = cur6;
-                        3'd7: nxt5 = cur7;
+                        3'd0: nxt5 = c0;    3'd1: nxt5 = c1;
+                        3'd2: nxt5 = c2;    3'd3: nxt5 = c3;
+                        3'd4: nxt5 = c4;    3'd5: nxt5 = c5;
+                        3'd6: nxt5 = c6;    3'd7: nxt5 = c7;
                     endcase
                 end
                 3'd6: begin
                     case (y)
-                        3'd0: nxt6 = cur0;
-                        3'd1: nxt6 = cur1;
-                        3'd2: nxt6 = cur2;
-                        3'd3: nxt6 = cur3;
-                        3'd4: nxt6 = cur4;
-                        3'd5: nxt6 = cur5;
-                        3'd6: nxt6 = cur6;
-                        3'd7: nxt6 = cur7;
+                        3'd0: nxt6 = c0;    3'd1: nxt6 = c1;
+                        3'd2: nxt6 = c2;    3'd3: nxt6 = c3;
+                        3'd4: nxt6 = c4;    3'd5: nxt6 = c5;
+                        3'd6: nxt6 = c6;    3'd7: nxt6 = c7;
                     endcase
                 end
                 3'd7: begin
                     case (y)
-                        3'd0: nxt7 = cur0;
-                        3'd1: nxt7 = cur1;
-                        3'd2: nxt7 = cur2;
-                        3'd3: nxt7 = cur3;
-                        3'd4: nxt7 = cur4;
-                        3'd5: nxt7 = cur5;
-                        3'd6: nxt7 = cur6;
-                        3'd7: nxt7 = cur7;
+                        3'd0: nxt7 = c0;    3'd1: nxt7 = c1;
+                        3'd2: nxt7 = c2;    3'd3: nxt7 = c3;
+                        3'd4: nxt7 = c4;    3'd5: nxt7 = c5;
+                        3'd6: nxt7 = c6;    3'd7: nxt7 = c7;
                     endcase
                 end
             endcase
-
-            // second: write y position with value from x
             case (y)
                 3'd0: begin
                     case (x)
-                        3'd0: nxt0 = cur0;
-                        3'd1: nxt0 = cur1;
-                        3'd2: nxt0 = cur2;
-                        3'd3: nxt0 = cur3;
-                        3'd4: nxt0 = cur4;
-                        3'd5: nxt0 = cur5;
-                        3'd6: nxt0 = cur6;
-                        3'd7: nxt0 = cur7;
+                        3'd0: nxt0 = c0;    3'd1: nxt0 = c1;
+                        3'd2: nxt0 = c2;    3'd3: nxt0 = c3;
+                        3'd4: nxt0 = c4;    3'd5: nxt0 = c5;
+                        3'd6: nxt0 = c6;    3'd7: nxt0 = c7;
                     endcase
                 end
                 3'd1: begin
                     case (x)
-                        3'd0: nxt1 = cur0;
-                        3'd1: nxt1 = cur1;
-                        3'd2: nxt1 = cur2;
-                        3'd3: nxt1 = cur3;
-                        3'd4: nxt1 = cur4;
-                        3'd5: nxt1 = cur5;
-                        3'd6: nxt1 = cur6;
-                        3'd7: nxt1 = cur7;
+                        3'd0: nxt1 = c0;    3'd1: nxt1 = c1;
+                        3'd2: nxt1 = c2;    3'd3: nxt1 = c3;
+                        3'd4: nxt1 = c4;    3'd5: nxt1 = c5;  
+                        3'd6: nxt1 = c6;    3'd7: nxt1 = c7;
                     endcase
                 end
                 3'd2: begin
                     case (x)
-                        3'd0: nxt2 = cur0;
-                        3'd1: nxt2 = cur1;
-                        3'd2: nxt2 = cur2;
-                        3'd3: nxt2 = cur3;
-                        3'd4: nxt2 = cur4;
-                        3'd5: nxt2 = cur5;
-                        3'd6: nxt2 = cur6;
-                        3'd7: nxt2 = cur7;
+                        3'd0: nxt2 =c0;    3'd1: nxt2 =c1;
+                        3'd2: nxt2= c2;    3'd3: nxt2 =c3;
+                        3'd4: nxt2 = c4;    3'd5: nxt2=c5;
+                        3'd6: nxt2 =c6;    3'd7: nxt2 = c7;
                     endcase
                 end
                 3'd3: begin
                     case (x)
-                        3'd0: nxt3 = cur0;
-                        3'd1: nxt3 = cur1;
-                        3'd2: nxt3 = cur2;
-                        3'd3: nxt3 = cur3;
-                        3'd4: nxt3 = cur4;
-                        3'd5: nxt3 = cur5;
-                        3'd6: nxt3 = cur6;
-                        3'd7: nxt3 = cur7;
+                        3'd0: nxt3 = c0;    3'd1: nxt3 = c1;
+                        3'd2: nxt3 = c2;    3'd3: nxt3 = c3;
+                        3'd4: nxt3 = c4;    3'd5: nxt3 = c5;
+                        3'd6: nxt3 = c6; 3'd7: nxt3 = c7;
                     endcase
                 end
                 3'd4: begin
                     case (x)
-                        3'd0: nxt4 = cur0;
-                        3'd1: nxt4 = cur1;
-                        3'd2: nxt4 = cur2;
-                        3'd3: nxt4 = cur3;
-                        3'd4: nxt4 = cur4;
-                        3'd5: nxt4 = cur5;
-                        3'd6: nxt4 = cur6;
-                        3'd7: nxt4 = cur7;
+                        3'd0: nxt4 = c0;    3'd1: nxt4 = c1;
+                        3'd2: nxt4 = c2;     3'd3: nxt4 = c3;
+                        3'd4: nxt4 = c4;    3'd5: nxt4 = c5;
+                        3'd6: nxt4 = c6;     3'd7: nxt4 = c7;
                     endcase
                 end
                 3'd5: begin
                     case (x)
-                        3'd0: nxt5 = cur0;
-                        3'd1: nxt5 = cur1;
-                        3'd2: nxt5 = cur2;
-                        3'd3: nxt5 = cur3;
-                        3'd4: nxt5 = cur4;
-                        3'd5: nxt5 = cur5;
-                        3'd6: nxt5 = cur6;
-                        3'd7: nxt5 = cur7;
+                        3'd0: nxt5 = c0;     3'd1: nxt5 = c1;
+                        3'd2: nxt5 = c2;    3'd3: nxt5 = c3;
+                        3'd4: nxt5 = c4;    3'd5: nxt5 = c5;
+                        3'd6: nxt5 = c6;    3'd7: nxt5 = c7;
                     endcase
                 end
                 3'd6: begin
                     case (x)
-                        3'd0: nxt6 = cur0;
-                        3'd1: nxt6 = cur1;
-                        3'd2: nxt6 = cur2;
-                        3'd3: nxt6 = cur3;
-                        3'd4: nxt6 = cur4;
-                        3'd5: nxt6 = cur5;
-                        3'd6: nxt6 = cur6;
-                        3'd7: nxt6 = cur7;
+                        3'd0: nxt6 = c0;    3'd1: nxt6 = c1;
+                        3'd2: nxt6 =c2;    3'd3: nxt6 = c3;
+                        3'd4: nxt6 =c4;    3'd5: nxt6 = c5;
+                        3'd6: nxt6 =c6;    3'd7: nxt6 = c7;
                     endcase
                 end
                 3'd7: begin
                     case (x)
-                        3'd0: nxt7 = cur0;
-                        3'd1: nxt7 = cur1;
-                        3'd2: nxt7 = cur2;
-                        3'd3: nxt7 = cur3;
-                        3'd4: nxt7 = cur4;
-                        3'd5: nxt7 = cur5;
-                        3'd6: nxt7 = cur6;
-                        3'd7: nxt7 = cur7;
+                        3'd0: nxt7 = c0;    3'd1: nxt7 = c1;
+                        3'd2: nxt7 = c2;    3'd3: nxt7 = c3;
+                        3'd4: nxt7 = c4;    3'd5: nxt7 = c5;
+                        3'd6: nxt7 = c6;    3'd7: nxt7 = c7;
                     endcase
                 end
             endcase
@@ -237,43 +156,28 @@ endmodule
 
 
 module rf_swap (
-    input  logic       clk,
-    input  logic       init,
-    input  logic       swapnow,
-    input  logic [2:0] x,
-    input  logic [2:0] y,
-    output logic [3:0] R0,
-    output logic [3:0] R1,
-    output logic [3:0] R2,
-    output logic [3:0] R3,
-    output logic [3:0] R4,
-    output logic [3:0] R5,
-    output logic [3:0] R6,
-    output logic [3:0] R7
+    input  logic clk,
+    input  logic init,
+    input  logic swapnow,
+    input  logic [2:0] x,[2:0] y,
+    output logic [3:0] R0, [3:0] R1,[3:0] R2, [3:0] R3,[3:0] R4,[3:0] R5, [3:0] R6, [3:0] R7
 );
 
-    logic [3:0] N0, N1, N2, N3, N4, N5, N6, N7;
+    logic [3:0] n0, n1, n2, n3, n4, n5, n6, n7;
 
-    rf_swap_next u_next (
-        .cur0(R0), .cur1(R1), .cur2(R2), .cur3(R3),
-        .cur4(R4), .cur5(R5), .cur6(R6), .cur7(R7),
-        .init(init),
-        .swapnow(swapnow),
-        .x(x),
-        .y(y),
-        .nxt0(N0), .nxt1(N1), .nxt2(N2), .nxt3(N3),
-        .nxt4(N4), .nxt5(N5), .nxt6(N6), .nxt7(N7)
+    rf_s_nxt u_next (
+        .init(init),.swapnow(swapnow),
+        .x(x),.y(y),
+        .c0(R0), .c1(R1), .c2(R2), .c3(R3),.c4(R4), .c5(R5), .c6(R6), .c7(R7),
+        .nxt0(n0), .nxt1(n1), .nxt2(n2), .nxt3(n3),.nxt4(n4), .nxt5(n5), .nxt6(n6), .nxt7(n7)
     );
 
     always_ff @(posedge clk) begin
-        R0 <= N0;
-        R1 <= N1;
-        R2 <= N2;
-        R3 <= N3;
-        R4 <= N4;
-        R5 <= N5;
-        R6 <= N6;
-        R7 <= N7;
+        R0 <= N0;    R1 <= N1;
+        R2 <= N2;    R3 <= N3;
+        R4 <= N4;    R5 <= N5;
+        R6 <= N6;    R7 <= N7;
     end
+
 
 endmodule
